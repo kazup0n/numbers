3784c3289100f82279b9c85ed1d141d04685a9a1
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2020-10-24 23:18:12 +0900

    パーツ追加方法と組み合わせ生成の修正

--- assets/numbers.js
+++ assets/numbers.js
@@ -1,38 +1,114 @@
 $(function () {
+  const partsInput = $('#parts-input')
+  const partsType = $('#parts-type-select')
+  const partsTable = $('#parts-table > tbody')
 
-  function extract_numbers_from(id) {
-    return $(id).find('input').map((_, t) => $(t).val())
-      .toArray()
-      .filter(n => !!n)
-      .map(n => Number(n))
-      .filter(n => !isNaN(n))
+  function appendParts(part, numbers) {
+    const cells = numbers.map(n => $(`<td>${n}</td>`))
+    const deleteMe = $(`<button type="button" class="btn btn-link">削除</button>`)
+    deleteMe.on('click', e => $(e.target).parent('tr').remove())
+    const row = $('<tr></tr>')
+    row.append(`<td>${part}</td>`)
+    row.append(cells)
+    row.append(deleteMe)
+    partsTable.append(row)
   }
 
-  $('#create-btn').on('click', () => {
-    const head = extract_numbers_from('#head-group')
-    const arm = extract_numbers_from('#arm-group')
-    const body = extract_numbers_from('#body-group')
-    const leg = extract_numbers_from('#leg-group')
-    const target = Number($('#target').val())
-    const results = [];
+  function getParts() {
+    return partsTable.find('tr').map(function () {
+      return {
+        type: $(this).find('td:eq(0)').text(),
+        params: [
+          Number($(this).find('td:eq(1)').text()),
+          Number($(this).find('td:eq(2)').text()),
+          Number($(this).find('td:eq(3)').text()),
+          Number($(this).find('td:eq(4)').text()),
+          Number($(this).find('td:eq(5)').text()),
+          Number($(this).find('td:eq(6)').text())
+        ]
+      }
+    }).toArray()
+  }
+
+  function generateTuples(parts) {
+    const { head, arm, body, leg } = _.groupBy(parts, 'type')
+    if (!_.every([head, arm, body, leg], _.isArray)) {
+      throw Error('全部位のパーツがありません')
+    }
+    const tuples = []
     for (let h = 0; h < head.length; h++) {
       for (let a = 0; a < arm.length; a++) {
         for (let b = 0; b < body.length; b++) {
           for (let l = 0; l < leg.length; l++) {
-            const sum = head[h] + arm[a] + body[b] + leg[l]
-            const score = Math.abs(target - sum)
-            const params = { h: head[h], a: arm[a], b: body[b], l: leg[l] }
-            results.push({ sum, score, params })
+            //パラメータごとの合計
+            const params = _.range(0, 6).map(i => {
+              return _.sum([
+                head[h].params[i],
+                arm[a].params[i],
+                body[b].params[i],
+                leg[l].params[i]
+              ])
+            })
+            tuples.push({
+              parts: {
+                head: head[h],
+                arm: arm[a],
+                body: body[b],
+                leg: leg[l]
+              },
+              params,
+              total: _.sum(params)
+            })
           }
         }
       }
-    }//end of for
-    results.sort((r1, r2) => r1.score - r2.score)
-    const resultTxt = results.slice(0, 5)
-      .map((r, i) => `<li>(計 ${r.sum}). 頭: ${r.params.h}, 腕: ${r.params.a}, 体: ${r.params.b}, 脚: ${r.params.l}</li>`)
-      .join('')
-    $('#result').empty().html(`<ol>${resultTxt}</ol>`)
-  })
-}
+    }
+    return tuples
+  }
 
-)
+  function resultRow(tuple) {
+    const parts = $(`<td>
+      頭: ${tuple.parts.head.params.join(' ')} <br />
+      腕: ${tuple.parts.arm.params.join(' ')} <br />
+      体: ${tuple.parts.body.params.join(' ')} <br />
+      脚: ${tuple.parts.leg.params.join(' ')}
+    </td>`)
+    const params = tuple.params.map(i => $(`<td>${i}</td>`))
+    const total = $(`<td>${tuple.total}</td>`)
+    const row = $('<tr></tr>')
+    row.append(parts)
+    row.append(total)
+    row.append(params[0])
+    row.append(params[1])
+    row.append(params[2])
+    row.append(params[3])
+    row.append(params[4])
+    row.append(params[5])
+    return row
+  }
+
+  $('#parts-add').on('click', (e) => {
+    //バリデーション
+    const v = partsInput.val()
+    const ns = v.split(' ').filter(n => n.length > 0).map(n => Number(n.trim())).filter(n => !isNaN(n))
+    if (ns.length != 6) {
+      partsInput.addClass('is-invalid')
+    } else {
+      appendParts(partsType.val(), ns)
+      partsInput.removeClass('is-invalid')
+    }
+  })
+
+  $('#parts-sort').on('click', () => {
+    const parts = getParts()
+    try {
+      const tuples = _.sortBy(generateTuples(parts), ['total']).reverse()
+      const tbl = $('#result-table > tbody')
+      tbl.empty()
+      tuples.forEach(t => tbl.append(resultRow(t)))
+    } catch (e) { 
+      alert(e.message)
+    }
+  })
+
+})
